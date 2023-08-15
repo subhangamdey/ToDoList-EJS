037ad7b1b80896c99d1d56207f7e338fe8a83750
Subhangam Dey <deysubhangam@gmail.com>
2023-08-15 14:36:50 +0530

    day 1 of making databases

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from "express"
 import bodyParser from "body-parser";
+import {MongoClient} from 'mongodb';
+import mongoose from "mongoose";
+const { Schema } = mongoose;
 
 const app = express();
 const port = 3000;
@@ -8,6 +11,17 @@ let inputArray = [];
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb+srv://subhangam03:Dhriti03%40@cluster0.49qfooz.mongodb.net/?retryWrites=true&w=majority/todolistDB");
+
+const blogSchema = new Schema({
+  name: String
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+const doc = new Blog();
+doc.name = 'foo';
+const documents = [doc];
 
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
